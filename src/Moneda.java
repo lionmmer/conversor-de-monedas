22689b5dac452b063a8d018c7b8344f178c3a735
lionmmer <lafl95@outlook.com>
2024-05-09 13:40:57 -0600

    Creación Moneda

---
+++ src/Moneda.java
@@ -0,0 +1,59 @@
+public class Moneda {
+    private String result;
+    private String documentation;
+    private String terms_of_use;
+    private int time_last_update_unix;
+    private String time_last_update_utc;
+    private int time_next_update_unix;
+    private String time_next_update_utc;
+    private String base_code;
+    private String conversion_rates;
+    private String initialBaseCode;
+
+    //CONSTRUCTOR
+    public Moneda(String initialBaseCode,String base_code) {
+        this.initialBaseCode = initialBaseCode;
+        this.base_code = base_code;
+    }
+
+    //GETTERS
+    public String getResult() {
+        return result;
+    }
+
+    public String getDocumentation() {
+        return documentation;
+    }
+
+    public String getTerms_of_use() {
+        return terms_of_use;
+    }
+
+    public String getTime_last_update_unix() {
+        return time_last_update_unix;
+    }
+
+    public String getTime_last_update_utc() {
+        return time_last_update_utc;
+    }
+
+    public int getTime_next_update_unix() {
+        return time_next_update_unix;
+    }
+
+    public String getTime_next_update_utc() {
+        return time_next_update_utc;
+    }
+
+    public String getBase_code() {
+        return base_code;
+    }
+
+    public String getConversion_rates() {
+        return conversion_rates;
+    }
+
+    public String getInitialBaseCode() {
+        return initialBaseCode;
+    }
+}
