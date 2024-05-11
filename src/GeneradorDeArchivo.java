662f6e92c9c4d0e73c8804d8a786592d1397d8b7
lionmmer <lafl95@outlook.com>
2024-05-11 15:20:15 -0600

    Creación GeneradorDeArchivo

---
+++ src/GeneradorDeArchivo.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GeneradorDeArchivo {
+    public void generarArchivo(Moneda moneda, String filePath) throws IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(moneda);
+        try (FileWriter writer = new FileWriter("moneda.json")) {
+            writer.write(json);
+        }
+    }
+}
